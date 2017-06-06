a7fcde597bb8cec5e922ef491f9e8b55fd1d7811
cluo1 <cluo1@LIBR100L-27.ND.EDU>
2017-06-06 16:07:41 -0400

    Update DemandBid, SupplyOffer, matcher from library!

--- src/centralmanagment/PlatformController.java
+++ src/centralmanagment/PlatformController.java
@@ -23,23 +23,25 @@ public class PlatformController {
 	 */
 	
 	public static final long min2hour = 60; // One hour is equal to 60 minutes
-	public static int testInterval = 6; 
+	public static int powerPlanRange = 10; // The range for power plan 
 	public static long standardTime = 0; // in minute
 	public static long timeInterval = 15; // in minute
 	public static int numInterval = 20; // The number of intervals
-	public static double pGenerate = 0.4; // the probability that this bus will generate a new bid or offer is 90%
+	public static double pGenerate = 0.5; // the probability that this bus will generate a new bid or offer is 90%
 	public static int timeRangeBid = 10; // the start time range used in Demand bid generation
 	public static int timeRangeOffer = 5; // the start time range used in supply offer generation
 	public static int minQuantity = 20; // Min electricity demand is 20 MWh
-	public static int maxQuantity = 50; // Max electricity demand is 200 MWh
+	public static int maxQuantity = 25; // Max electricity demand is 200 MWh
 	public static int bidid = 0; // global bid id counter
 	public static int offerid = 0; // global offer id counter
 	public static Semaphore bididlock = new Semaphore(1); // lock for bids
 	public static Semaphore offeridlock = new Semaphore(1); // lock for offer;
-	public static double sourcePriceBid = 10; // 
-	public static double sourcePriceOffer = 9; 
+	public static double maxSourcePriceBid = 10; // Max source price for bid 
+	public static double minSourcePriceBid = 5; // Min source price for bid 
+	public static double maxSourcePriceOffer = 10; // Max source price for offer
+	public static double minSourcePriceOffer = 5; // Min source price for offer
 	public static double deliverPrice = 10; // 
-	public static int maxRoute = 3; // The max number of routes returned
+	public static int maxRoute = 4; // The max number of routes returned
 	
 	public NetworkGraph network;
 	public SupplyDemandMatcher matcher;
